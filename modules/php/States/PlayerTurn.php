c5657b3c475c4a3cfecf291309ff2a6119346161
Jay Sachs <jaysachs@users.noreply.github.com>
2025-11-03 07:55:04 -0500

    Fix can_draw computation

--- modules/php/States/PlayerTurn.php
+++ modules/php/States/PlayerTurn.php
@@ -68,7 +68,7 @@ class PlayerTurn extends AbstractState
 		}));
 		*/
 		return [
-			'can_draw' => $model->getDeck()->drawn != null,
+			'can_draw' => $model->getDeck()->drawn == null,
 			'can_purchase' => $player->available_extensions > 0,
 			'can_buy' => false,
 			'can_swap' => false,
