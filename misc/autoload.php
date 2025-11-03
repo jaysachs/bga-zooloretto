6d84a16ce5a7db5b138f64daa420221f3f122da3
Jay Sachs <jaysachs@users.noreply.github.com>
2025-11-03 07:44:52 -0500

    fix autoload

--- misc/autoload.php
+++ misc/autoload.php
@@ -3,8 +3,8 @@
 require_once('work/test/module/table/table.game.php');
 
 spl_autoload_register(function ($class) {
-    if (str_starts_with($class, 'Bga\\Games\\babylonia\\')) {
-        $file = 'modules/php/' . str_replace('\\', '/', substr($class, 20)) . '.php';
+    if (str_starts_with($class, 'Bga\\Games\\zooloretto\\')) {
+        $file = 'modules/php/' . str_replace('\\', '/', substr($class, 21)) . '.php';
         if (file_exists($file)) {
             require $file;
             return true;
