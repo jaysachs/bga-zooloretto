7eac15acf4d4bec4ae7ef2287ac70d317a3adf6c
Jay Sachs <jay@covariant.org>
2026-02-10 14:59:16 -0500

    Merge FlowState into PlayFlow.

--- src/flow.ts
+++ src/flow.ts
@@ -16,161 +16,50 @@ type Continuation = {
 }
 
 function strElem(el: HTMLElement | undefined): string {
-    if (!el) { return "undefined"; }
-    var elem : HTMLElement | null = el;
-    var s = "";
-    while (elem) {
-      if (elem.id) { return "#" + elem.id + s; }
-      s = ">" + elem.tagName + s;
-      elem = elem.parentElement;
-    }
-    return s;
+  if (!el) { return "undefined"; }
+  var elem : HTMLElement | null = el;
+  var s = "";
+  while (elem) {
+    if (elem.id) { return "#" + elem.id + s; }
+    s = ">" + elem.tagName + s;
+    elem = elem.parentElement;
   }
+  return s;
+}
 
 
 type OpList = Op[];
 
-class FlowState {
-    // FIXME: this needs to be part of this, but it shouldn't be exposed.
-    marked: HTMLElement[] = [];
-
-    private ops: NamedOp[] = [];
-    private continuations: Continuation[] = [];
-    private onClickAbortController : AbortController = new AbortController();
-    private current: Continuation | undefined;
-    private readonly consumer: (OpList) => any;
-    constructor(consumer: (OpList) => any) {
-      this.consumer = consumer;
-    }
-
-    private async undoTo(mark: number) {
-      const anims: OpList = [];
-      while (this.ops.length > mark) {
-        anims.push(this.ops.pop()!.op);
-      }
-      await this.consumer(anims);
-    }
-
-    private remove(x: Continuation) {
-      console.debug("flowState remove", x.op.desc);
-      // FIXME: can change to allow "jump" undos
-      this.current = this.continuations.pop();
-      if (this.current !== x) {
-        console.error("Undo remove expected top of ", x, "but found", this.current);
-      }
-    }
-
-    pushOp(op : NamedOp): void {
-      this.ops.push(op);
-    }
-
-    undo() : Op | undefined {
-      console.debug("flowState undo");
-      const x = this.continuations.at(-1);
-      if (x) {
-        console.debug("   flowState undo ", x.op.desc);
-        // this.current = x;
-        // FIXME: ?? this should remove things when it actually fires?
-        return async () => { this.remove(x); await this.undoTo(x.mark).then(() => x.op.op()) };
-      } else {
-        console.error("Nowhere to undo to!");
-        return undefined;
-      }
-    }
-
-    async callUndoably(desc: string, thing: Op) {
-      console.debug("flowState callUndoably", desc);
-      if (this.current !== undefined) {
-        console.debug("flowState pushing", this.current);
-        this.continuations.push(this.current);
-      }
-      this.current = {op: { desc: desc, op: thing }, mark: this.ops.length};
-      console.debug("flowState new current: ", this.current);
-      await thing();
-    }
-
-    clear() {
-        console.debug("flowState clear");
-        this.current = undefined;
-        this.continuations = [];
-        this.ops = [];
-        this.clearMarked();
-        this.resetController();
-    }
-
-    resetController() {
-      console.debug("flowState resetController");
-      this.onClickAbortController.abort();
-      this.onClickAbortController = new AbortController();
-    }
-
-    abortSignal(): AbortSignal {
-      return this.onClickAbortController.signal;
-    }
-
-  clearMarked() {
-    console.debug("clearMarked");
-    while (this.marked.length > 0) {
-      const elem = this.marked.pop()!;
-      if (!this.inUndo) {
-        console.debug("clearMarked **AS UNDOABLE OP**", elem);
-        const m = elem.getAttribute(Attrs.MARK);
-        this.pushOp({
-          desc: `clearMarkedNotUndo:${strElem(elem)}:[${m}]`,
-          op: async () => elem.setAttribute(Attrs.MARK, m)
-        })
-      }
-      console.debug("clearing marked", elem);
-      elem.title = '';
-      elem.removeAttribute(Attrs.MARK);
-    }
-  }
-
-  private inUndo: boolean = false;
-  async rollback() {
-    console.debug("***");
-    console.debug("flowstate rollback");
-    // this.clearMarked();
-    this.inUndo = true;
-    this.resetController();
-    await this.undoTo(0).then(this.clear.bind(this)).then(() => this.inUndo = false );
-  }
-}
-
 export abstract class PlayFlow<T> {
-  private static lastId: number = 0;
-  protected id: number;
+  private ops: NamedOp[] = [];
+  private continuations: Continuation[] = [];
+  private onClickAbortController : AbortController = new AbortController();
+  private current: Continuation | undefined;
+  private readonly consumer: (OpList) => any;
+  private inUndo: boolean = false;
+  private readonly marked: HTMLElement[] = [];
+
   protected readonly animationManager: AnimationManager;
   protected readonly bga: Bga;
-  protected flowState: FlowState;
   protected player_id: number;
 
-  constructor(animationManager: AnimationManager, bga: Bga) {
+  protected constructor(animationManager: AnimationManager, bga: Bga) {
     this.animationManager = animationManager;
     this.bga = bga;
-    this.flowState = new FlowState(animationManager.playSequentially);
-    this.id = PlayFlow.lastId++;
-  }
-
-  protected useAutoclick(): boolean {
-    return false;
-  }
-
-  protected pushUndoOp(desc: string, anim: Op): void {
-    this.flowState.pushOp({ desc: desc, op: anim });
+    this.consumer = animationManager.playSequentially;
   }
 
   public onLeavingState(args: T, isCurrentPlayerActive: boolean) {
     console.log("onLeavingState", (this as any).constructor?.name, args, isCurrentPlayerActive);
     if (isCurrentPlayerActive) {
-      // this.flowState.clear();
+      // this.clear();
     }
   }
 
   public onEnteringState(args: T, isCurrentPlayerActive: boolean) {
     console.log("onEnteringState", (this as any).constructor?.name, args, isCurrentPlayerActive);
     if (isCurrentPlayerActive) {
-      // this.flowState.clear();
+      // this.clear();
       console.log("start:", (this as any).constructor?.name);
       this.player_id = this.bga.gameui.player_id;
       const desc = "Start " + (this as any).constructor?.name;
@@ -178,17 +67,28 @@ export abstract class PlayFlow<T> {
     }
   }
 
-  protected async callUndoably(desc: string, thing: () => Promise<any>) {
-    this.flowState.callUndoably(desc, thing);
-  }
-
   protected abstract start(args?: T);
 
-  protected async playParallel(anims: OpList) {
-    await this.animationManager.playParallel(anims);
-  }
+  protected abstract useAutoclick(): boolean;
+
+  protected abstract confirmationsEnabled(): boolean;
 
   protected abstract offboard(): HTMLElement | undefined;
+
+  protected pushUndoOp(desc: string, anim: Op): void {
+    this.pushOp({ desc: desc, op: anim });
+  }
+
+ protected async callUndoably(desc: string, thing: Op) {
+    console.debug("flowState callUndoably", desc);
+    if (this.current !== undefined) {
+      console.debug("flowState pushing", this.current);
+      this.continuations.push(this.current);
+    }
+    this.current = {op: { desc: desc, op: thing }, mark: this.ops.length};
+    console.debug("flowState new current: ", this.current);
+    await thing();
+  }
 
   protected async slide(elem: HTMLElement, newParent: HTMLElement) {
     const currParent = elem.parentElement as HTMLElement;
@@ -213,20 +113,14 @@ export abstract class PlayFlow<T> {
     await this.animationManager.slideOutAndDestroy(elem, toElem, {});
   }
 
-  protected async rollback() {
-    await this.flowState.rollback();
-  }
-
   protected initStatusBar(title: string, args?: any) {
     this.bga.statusBar.removeActionButtons();
     this.bga.statusBar.setTitle(title, args);
   }
 
-  protected abstract confirmationsEnabled(): boolean;
-
   protected async addConfirmAndRestartActionButtons(bgaAction: string, args: any, settings?: {restart?: () => Promise<any>, post?: () => Promise<any>}) {
     const doAct = async () => {
-        this.flowState.clearMarked();
+        this.clearMarked();
         await this.bga.actions.performAction(bgaAction, args).then(settings?.post);
     };
     if (!this.confirmationsEnabled())  {
@@ -256,7 +150,7 @@ export abstract class PlayFlow<T> {
       { color: "secondary"});
 
     /*
-    const undoReturn = this.flowState.undo();
+    const undoReturn = this.undo();
     if (undoReturn) {
       this.bga.statusBar.addActionButton(_('Undo'),
         async () => {
@@ -264,26 +158,17 @@ export abstract class PlayFlow<T> {
             confirm.disabled = true;
             confirm.remove();
           }
-          this.clearOnclicks(); await undoReturn()
+          this.resetController(); await undoReturn()
         },
         { color: "secondary"});
     }
     */
   }
 
+  // FIXME: shouldn't need to expose this?
+  //   but need to allow butons to cancel onclicks.
   protected clearOnclicks(): void {
-    this.flowState.resetController();
-  }
-
-  private mark(elem: HTMLElement | undefined, mark: 'selected' | 'selectable' | 'moved' ): void {
-    if (!elem) {
-      return;
-    }
-    console.debug("mark", elem, mark);
-    this.flowState.marked.push(elem);
-    const m = elem.getAttribute(Attrs.MARK);
-    elem.setAttribute(Attrs.MARK, mark);
-    this.pushUndoOp(`mark:${mark}:${m} ${strElem(elem)}`, async () => elem.setAttribute(Attrs.MARK, m));
+    this.resetController();
   }
 
   protected markSelected(elem: HTMLElement | undefined) {
@@ -307,15 +192,106 @@ export abstract class PlayFlow<T> {
       "click",
       async (ev: MouseEvent) => {
         console.debug(`clicked on ${strElem(elem)}`);
-        this.clearOnclicks();
-        this.flowState.clearMarked();
+        this.resetController();
+        this.clearMarked();
         this.markSelected(elem);
         await onclick(ev);
       },
-      { signal: this.flowState.abortSignal() });
+      { signal: this.abortSignal() });
   }
 
-  protected getPlayerPanelElement(player_id: number): HTMLElement {
-    return this.bga.playerPanels.getElement(player_id);
+  private clear() {
+    console.debug("flowState clear");
+    this.current = undefined;
+    this.continuations = [];
+    this.ops = [];
+    this.resetController();
   }
+
+  private async undoTo(mark: number) {
+    const anims: OpList = [];
+    while (this.ops.length > mark) {
+      anims.push(this.ops.pop()!.op);
+    }
+    await this.consumer(anims);
+  }
+
+  private resetController() {
+    console.debug("flowState resetController");
+    this.onClickAbortController.abort();
+    this.onClickAbortController = new AbortController();
+  }
+
+  private abortSignal(): AbortSignal {
+    return this.onClickAbortController.signal;
+  }
+
+  private pushOp(op : NamedOp): void {
+    this.ops.push(op);
+  }
+
+  /*
+  private remove(x: Continuation) {
+    console.debug("flowState remove", x.op.desc);
+    // FIXME: can change to allow "jump" undos
+    this.current = this.continuations.pop();
+    if (this.current !== x) {
+      console.error("Undo remove expected top of ", x, "but found", this.current);
+    }
+  }
+
+  undo() : Op | undefined {
+    console.debug("flowState undo");
+    const x = this.continuations.at(-1);
+    if (x) {
+      console.debug("   flowState undo ", x.op.desc);
+      // this.current = x;
+      // FIXME: ?? this should remove things when it actually fires?
+      return async () => { this.remove(x); await this.undoTo(x.mark).then(() => x.op.op()) };
+    } else {
+      console.error("Nowhere to undo to!");
+      return undefined;
+    }
+  }
+  */
+
+  private mark(elem: HTMLElement | undefined, mark: 'selected' | 'selectable' | 'moved' ): void {
+    if (!elem) {
+      return;
+    }
+    console.debug("mark", elem, mark);
+    this.marked.push(elem);
+    const m = elem.getAttribute(Attrs.MARK);
+    elem.setAttribute(Attrs.MARK, mark);
+    this.pushUndoOp(`mark:${mark}:${m} ${strElem(elem)}`, async () => elem.setAttribute(Attrs.MARK, m));
+  }
+
+  private async rollback() {
+    this.clearMarked();
+    console.debug("***");
+    console.debug("   rollback");
+    // this.clearMarked();
+    this.inUndo = true;
+    this.resetController();
+    await this.undoTo(0).then(this.clear).then(() => this.inUndo = false );
+  }
+
+  private clearMarked() {
+    console.debug("clearMarked");
+    while (this.marked.length > 0) {
+      const elem = this.marked.pop()!;
+      if (!this.inUndo) {
+        console.debug("clearMarked **AS UNDOABLE OP**", elem);
+        const m = elem.getAttribute(Attrs.MARK);
+        this.pushOp({
+          desc: `clearMarkedNotUndo:${strElem(elem)}:[${m}]`,
+          op: async () => elem.setAttribute(Attrs.MARK, m)
+        })
+      }
+      console.debug("clearing marked", elem);
+      elem.title = '';
+      elem.removeAttribute(Attrs.MARK);
+    }
+  }
+
 }
