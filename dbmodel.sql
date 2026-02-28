7187334b62d273ad523fd5de49a7aa2357ba09ec
Jay Sachs <jay@covariant.org>
2026-02-28 08:53:19 -0500

    Avoid deprecated MySQL declarations.

--- dbmodel.sql
+++ dbmodel.sql
@@ -10,7 +10,7 @@
 
 CREATE TABLE IF NOT EXISTS `tiles` (
   -- child IDs = 10000 * motherid + fatherid
-  `id` INT(10) UNSIGNED NOT NULL,
+  `id` INT UNSIGNED NOT NULL,
   -- The tile type:
   --  C: camel generic
   --  CM: camel male
@@ -41,17 +41,17 @@ CREATE TABLE IF NOT EXISTS `tiles` (
   --  X: {salt_pos}
   `location` VARCHAR(1) NOT NULL,
   -- ignored unless E
-  `player_id` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+  `player_id` INT UNSIGNED NOT NULL DEFAULT 0,
   -- truck_id if T, enc_id if E
-  `loc_id` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+  `loc_id` INT UNSIGNED NOT NULL DEFAULT 0,
   -- truck_pos if T, enc_pos if E, stock_pos if S
-  `loc_pos` INT(10) UNSIGNED NOT NULL DEFAULT 0,
+  `loc_pos` INT UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   -- Requires uniquifying deleted / temporary positions
   --  we can use id offsets to accomplish this
   UNIQUE(`location`, `player_id`, `loc_id`, `loc_pos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `player` ADD COLUMN `money` int(10) unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `player` ADD COLUMN `purchased_extensions` int(10) unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `player` ADD COLUMN `truck_taken` int(10) unsigned NOT NULL DEFAULT 0;
+ALTER TABLE `player` ADD COLUMN `money` INT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `player` ADD COLUMN `purchased_extensions` INT UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `player` ADD COLUMN `truck_taken` INT UNSIGNED NOT NULL DEFAULT 0;
