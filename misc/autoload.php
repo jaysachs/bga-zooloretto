df3ac89a46d62bdf6e5ba3aa86a539675751a984
Jay Sachs <jay@covariant.org>
2026-05-06 09:20:25 -0400

    Fix type warning.

--- misc/autoload.php
+++ misc/autoload.php
@@ -2,7 +2,7 @@
 
 require_once('work/module/table/table.game.php');
 
-spl_autoload_register(function ($class) {
+spl_autoload_register(function (string $class) {
     $prefix = 'Bga\\Games\\zooloretto\\';
     if (str_starts_with($class, $prefix)) {
         $file = 'modules/php/' . str_replace('\\', '/', substr($class, strlen($prefix))) . '.php';
