f03f078e43a92158e309c1decdd8e7b86d0f2b83
Jay Sachs <jay@covariant.org>
2026-05-12 09:37:10 -0400

    Add license to TS files.

--- src/loadDrawnTileFlow.ts
+++ src/loadDrawnTileFlow.ts
@@ -1,3 +1,26 @@
+/**
+ *------
+ * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
+ * Zooloretto implementation : © Jay Sachs <vagabond@covariant.org>
+ *
+ * Copyright 2025 Jay Sachs <vagabond@covariant.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+ * See http://en.boardgamearena.com/#!doc/Studio for more information.
+ */
+
 import { ZooFlow } from "./zflow";
 import { Tile, TruckLocation } from "./zgametypes";
 import { Elements, IDS } from "./zhtml";
